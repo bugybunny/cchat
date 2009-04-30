93fe35d31087e122eff2fc7845b5404ee4b06534
www.bansa <www.bansa@localhost>
2009-04-30 09:46:00 +0000

    JavaScript-Funktionen, Eingabefeld f?r Text und Anzeige der Antwort

--- source/index.php
+++ source/index.php
@@ -1,33 +1,29 @@
 <?php
 	require "config.inc.php";
-	require "index.html";
 	
-	echo "<!DOCTYPE html PUBLIC '-//W3C//DTD HTML 4.01 Strict//EN' 'http://www.w3.org/TR/html4/strict.dtd'>
+	echo "
+<!DOCTYPE html PUBLIC '-//W3C//DTD HTML 4.01 Strict//EN' 'http://www.w3.org/TR/html4/strict.dtd'>
 <html>
 <head>
-<meta http-equiv='Content-Type' content='text/html; charset=UFT-8'>
-<title>cchat $cchat_version</title>
+	<meta http-equiv='Content-Type' content='text/html; charset=UFT-8'>
+	<title>cchat $cchat_version</title>
+	<script type='text/javascript' src='javascript/mootools-core.js'></script>
+	<script type='text/javascript' src='javascript/script.js'></script>
 </head>
 <body>
-	<form action='ajax.php' method='post'>
-		<textarea rows='20' cols='50' name='data'>{
-  'register': {
-    'name': 'hansmeier',
-    'password': 'pwd203',
-    'email': 'hans@meier.com'
-  },
-  'login': {
-    'name': 'hansmeier',
-    'password': 'pwd203'
-  },
-  'messages': [
-    'Hi everybody',
-    'Nice to see you',
-    'Bye!'
-  ],
-  'last': 1238549349
-}</textarea><br>
-		<input type='submit' value='testen'>
+	<form id='form' action='ajax.php' method='post'>
+		<p>
+			<label for='text'>Text</label>: <input type='text' length='30' id='text' name='text'>
+			<input type='submit' value='senden'>
+		</p>
+		<p>
+			<label for='data'>Antwort</label>:<br>
+			<textarea rows='20' cols='50' id='data' name='data'></textarea>
+		</p>
+		
+		<p>
+			<input type='submit' id='aktualisieren' name='aktualisieren' value='aktualisieren'>
+		</p>
 	</form>
 </body>
 </html>";
