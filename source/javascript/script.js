dd3ab36032cef727132d2973bce744c1a5ab930c
www.bansa <www.bansa@localhost>
2009-05-07 07:46:24 +0000

    Ge?ndert auf JSON und Seite neuladen verhindert

--- source/javascript/script.js
+++ source/javascript/script.js
@@ -1,14 +1,15 @@
 document.addEvent('domready', function() {
-	var ajax = new Request({
+	var ajax = new Request.JSON({
 		'url': 'ajax.php',
 		'data': '{}',
 		'link': 'chain',
 		'noCache': true
 	});
-	ajax.addEvent('success', function(text) {
+	ajax.addEvent('success', function(json, text) {
 		$('data').set('value', text);
 	});
-	$('form').addEvent('submit', function() {
+	$('form').addEvent('submit', function(event) {
+		event.stop();
 		var data = {
 			"login": {
 				"name": "hans",
