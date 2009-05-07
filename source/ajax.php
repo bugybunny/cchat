c17779a64e56dade3e644cc2ed49f35798ba2e13
marco.syfrig <marco.syfrig@localhost>
2009-05-07 07:32:10 +0000

    Login, Passwort- und Username?berprufung

--- source/ajax.php
+++ source/ajax.php
@@ -7,7 +7,9 @@
 // | Autor         Marco Syfrig (syfm)                                                            |
 // | Datum         2009-04-30                                                                     |
 // |                                                                                              |
-// | Beschreibung   			                                                      |
+// | Beschreibung  Erhält von index.php ein JSON dekodiertes Array $data per POST und wertet es   |
+// |               aus.                                                                           |
+// |               Es wird ein neues Array $data_answer erstellt. NOCH ERWEITERN                  |
 // |                                                                                              |
 // | Version  Datum       Beschreibung                                                  Autor     |
 // | -------  ----------  ------------                                                  -----     |
@@ -15,9 +17,11 @@
 // |                                                                                              |
 // +----------------------------------------------------------------------------------------------+
 include 'config.inc.php';
-
 session_start();
-header('Content-type: text/json; charset=utf-8');
+/* header('Content-type: text/json; charset=utf-8'); */
+mysql_connect("localhost", "root", "gibbiX12345");
+mysql_select_db("cchat");
+
 
 $data = json_decode($_POST['data'], true);
 
@@ -30,10 +34,38 @@ $data_answer['messages'][1]['message'] = "Hallo Hans";
 $data_answer['messages'][1]['time'] = floor(microtime(true) * 1000);
 
 /* User die sich ein- oder ausgeloggt haben in der Zeit seit dem letzten Senden */
-$data_answer['login'][0] = $data['login']['name']; 
-$data_answer['logout'][0] = "Rolf"; 
+$data_answer['login'][0] = $data['login']['name'];
+$data_answer['logout'][0] = "Rolf";
 $data_answer['logedin'][0] = true;
 $data_answer['logedin'][1] = false;
-/* echo json_encode($data_answer); */
 
+/* Login */
+if(isset($data['login'])) {
+	/* Username und Passwort überprüfen */
+	if(!empty($data['login']['name']) && !empty($data['login']['password'])) {
+		/* Datenbankabfrage machen */
+		$name = mysql_escape_string($data['login']['name']);
+		$result = mysql_query("SELECT id, salt FROM user WHERE name = '$name'");
+		/* User wurde gefunden */
+		if(mysql_num_rows($result)) {
+			/* Passwort überprüfen */
+			$user = mysql_fetch_assoc($result);
+			$password = hash("sha256", $user['salt'] . hash("sha256", $user['salt'] . $data['login']['password']));
+			$result = mysql_select("SELECT id FROM user WHERE id = '{$user['id']}' AND password = '$password'");
+			if(mysql_num_rows($result)) {
+				$data_answer['login'][0] = $data['login']['name'];
+			}
+		}
+	} else {
+		
+	}
+}
+
+/* Registrierung */
+if(isset($data['register'])) {
+	/* Neuer Benutzer mit Passwort in der Datenbank anlegen */
+
+
+
+}
 ?>
