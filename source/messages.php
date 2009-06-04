b943005fd9d995e3c7701f9c88b4fc86d4f7f884
marco.syfrig <marco.syfrig@localhost>
2009-06-04 08:36:31 +0000

    Automatisches Login bei Registrierung; Nachrichten werden korrekt in die Datenbank gespeichert und zur?ckgeschickt; Passwort?berpr?fung bei Login

--- source/messages.php
+++ source/messages.php
@@ -3,33 +3,16 @@
  * Speichert die Nachrichten in der Datenbank.
  * @param string	$data
  */
-function message($data) {
+function insertmessages($data) {
 	$codes["message"] = 10;
 	$codes["login"]   = 20;
 	$codes["logout"]  = 30;
-	
+
 	if(isset($_SESSION['userid'])) {
 		foreach($data['messages'] as $message) {
 			$message = mysql_real_escape_string($message);
 			mysql_query("INSERT INTO action (typ, text, userid, time) VALUES ('{$codes['message']}', '$message', {$_SESSION['userid']}, ".floor(microtime(true) * 1000).")");
 			echo mysql_error();
-			
-			// TODO Zeitabfrage anpassen, sobald bei $data eine Time mitgesendet wird 
-			$result_message = mysql_query("SELECT name FROM action WHERE typ = '{$codes['message']}' AND time >= 1");
-			if(!mysql_num_rows($result_message)) {
-				$action = mysql_fetch_assoc($result_message);
-				
-				/*
-				 * Da in action nur die UserID gespeichert ist, muss der Username über die Tabelle user geholt werden
-				 * um den Sendernamen in $data_answer['messages][x]['sender'] zu setzen
-				 */
-				$result_user = mysql_query("SELECT name FROM user WHERE id = {$action['userid']}");
-				// TODO $data_answer['messages'] setzen. Nachrichten, Login und Logout setzen
-				if(!mysql_num_rows($result_user)) {
-					
-				}
-				
-			}
 		}
 	}
 	/* Aktuell ist kein User eingeloggt. Deshalb können keine Nachrichten verschickt werden */
@@ -37,4 +20,29 @@ function message($data) {
 		echo "Bitte einloggen!";
 	}
 }
+
+/**
+ * Prüft ob seit der letzten Anfrage eine neue Nachricht / neue Nachrichten geschrieben wurde
+ * @return Array[][] $newmessages
+ * 			Neue Nachrichten mit Sender, Nachrichtentext und Zeit der Nachricht.
+ * 			Die Arraystruktur ist unter Answer beschrieben: http://code.google.com/p/cchat/wiki/Datenaustausch
+ *
+ */
+function checkNewMessages() {
+	// TODO Zeitabfrage anpassen, sobald bei $data eine Time mitgesendet wird
+	$result_message = mysql_query("SELECT u.name, a.text FROM action a, user u WHERE a.typ = {$codes['message']} AND a.time >= 1 AND a.id = u.id");
+	echo mysql_error();
+	if(!mysql_num_rows($result_message)) {
+		// TODO Login und Logout setzen
+		$action = mysql_fetch_assoc($result_message);
+		foreach($action as $row) {
+			$result[]['sender'] = $row['name'];
+			$result[]['message'] = $row['text'];
+			$result[]['time'] = floor(microtime() / 1000);
+		}
+		return $result;
+	}
+
+}
+
 ?>
--- source/register.php
+++ source/register.php
@@ -14,13 +14,24 @@ function register($name, $password, $mail) {
 		$name_register = mysql_real_escape_string($name);
 		$email_register = mysql_real_escape_string($mail);
 		$result_register = mysql_query("SELECT name FROM user WHERE name = '$name_register'");
-		/* Der Username existiert noch nicht, deshalb liefert die MySQL-Abfrage kein Ergebnis. Der Account kann erstellt werden */
+		/*
+		 * Der Username existiert noch nicht, deshalb liefert die MySQL-Abfrage kein Ergebnis.
+		 * Der Account kann erstellt werden
+		 */
 		if(!mysql_num_rows($result_register)) {
 			$salt = rand(1, PHP_INT_MAX) . "cchatisttoll" . rand(1, PHP_INT_MAX);
 			$password_register = hash("sha256", $salt . hash("sha256", $salt . $password));
 			/* Neuen user Daten */
 			mysql_query("INSERT INTO user (name, password, salt, mail, register, logedin, lastrefresh) VALUES ('$name_register', '$password_register', '$salt', '$email_register', now(), true, now())");
+			
+			/* User automatisch einloggen nach der Registrierung */
+			require 'login.php';
+			login($name_register, $password);
+			
 			echo mysql_error();
+		} else {
+			/* Errorcode: Benutzer bereits vorhanden */
+			return 301;
 		}
 	}
 }
