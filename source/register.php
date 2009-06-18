d6e4c28806adb1df2db990c8916333e982bab0c7
marco.syfrig <marco.syfrig@localhost>
2009-06-18 08:54:40 +0000

    Codedoku erweitert User die sich ein- oder ausgeloggt haben werden mitgeschickt

--- source/register.php
+++ source/register.php
@@ -9,8 +9,6 @@
  * @return int					Errorcode: Beschreibung der Codes unter http://code.google.com/p/cchat/wiki/Datenaustausch
  */
 function register($name, $password, $mail) {
-	$_SESSION['name'] = $name;	
-
 	if(!empty($name) && !empty($password) && !empty($mail)) {
 		$name_register = mysql_real_escape_string($name);
 		$email_register = mysql_real_escape_string($mail);
@@ -22,14 +20,12 @@ function register($name, $password, $mail) {
 		if(!mysql_num_rows($result_register)) {
 			$salt = rand(1, PHP_INT_MAX) . "cchatisttoll" . rand(1, PHP_INT_MAX);
 			$password_register = hash("sha256", $salt . hash("sha256", $salt . $password));
-			/* Neuen user Daten */
 			mysql_query("INSERT INTO user (name, password, salt, mail, register, logedin, lastrefresh) VALUES ('$name_register', '$password_register', '$salt', '$email_register', now(), true, now())");
 			echo mysql_error();
 			
 			/* User automatisch einloggen nach der Registrierung */
 			require 'login.php';
 			login($name_register, $password);
-			
 			echo mysql_error();
 		} 
 		/* Errorcode: Benutzer bereits vorhanden */
