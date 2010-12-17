30a96da07aadcd3e51c52b438de100c6b412c45a
jannis <jannis@localhost>
2010-12-17 18:48:01 +0100

    Removed LABjs - seems to be faster without that.

--- source/index.php
+++ source/index.php
@@ -15,45 +15,42 @@ header("Content-type: text/html; charset=utf-8");
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <title>cchat <?php echo CCHAT_VERSION ?></title>
         <link rel="stylesheet" href="css/style.css">
-        <script src="javascript/LAB.js"></script>
-        <script>
-            $LAB.script("javascript/mootools-core.js").wait()
-            .script("javascript/xhr.class.js")
-            .script("javascript/login.class.js")
-            .script("javascript/chat.class.js").wait()
-            .script("javascript/scripts.js");
-        </script>
+        <script src="javascript/mootools-core.js"></script>
+        <script src="javascript/xhr.class.js"></script>
+        <script src="javascript/login.class.js"></script>
+        <script src="javascript/chat.class.js"></script>
+        <script src="javascript/scripts.js"></script>
     </head>
     <body>
         <noscript><p>You need JavaScript to access cchat.</p></noscript>
-        <section id="login">
-            <form id="loginform" action="" method="post">
-                <div>
-                    <label for="name">Name</label>: <input id="name" type="text" name="name"><br>
-                    <label for="name">Passwort</label>: <input id="password" type="password" name="password">
-                </div>
-                <div id="register">
-                    <label for="name">Wiederholen</label>: <input id="password2" type="password" name="password2"><br>
-                    <label for="name">E-Mail</label>: <input id="email" type="text" name="email"><br>
-                </div>
-                <div>
-                    <input id="loginsubmit" type="submit" value="Login"> <a id="registertoggle" href="">Registrieren</a>
-                </div>
-            </form>
-        </section>
-        <section id="chat">
-            <div id="chatmessages"></div>
-            <div id="chatuser">
-                <p>User online:</p>
-                <ul id="chatuserlist"></ul>
+    <section id="login">
+        <form id="loginform" action="" method="post">
+            <div>
+                <label for="name">Name</label>: <input id="name" type="text" name="name"><br>
+                <label for="name">Passwort</label>: <input id="password" type="password" name="password">
             </div>
-            <form id="chatform" action="" method="post">
-                <p>
-                    <input id="chattext" type="text" name="text" size="100" autocomplete="off">
-                    <input type="submit" value="Go">
-                    <input id="chatlogout" type="button" name="logout" value="Logout">
-                </p>
-            </form>
-        </section>
+            <div id="register">
+                <label for="name">Wiederholen</label>: <input id="password2" type="password" name="password2"><br>
+                <label for="name">E-Mail</label>: <input id="email" type="text" name="email"><br>
+            </div>
+            <div>
+                <input id="loginsubmit" type="submit" value="Login"> <a id="registertoggle" href="">Registrieren</a>
+            </div>
+        </form>
+    </section>
+    <section id="chat">
+        <div id="chatmessages"></div>
+        <div id="chatuser">
+            <p>User online:</p>
+            <ul id="chatuserlist"></ul>
+        </div>
+        <form id="chatform" action="" method="post">
+            <p>
+                <input id="chattext" type="text" name="text" size="100" autocomplete="off">
+                <input type="submit" value="Go">
+                <input id="chatlogout" type="button" name="logout" value="Logout">
+            </p>
+        </form>
+    </section>
 </body>
 </html>
